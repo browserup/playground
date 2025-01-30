f17a4bf3151f583231cec91948d0f8c289493d71
Eric Beland <ebeland@gmail.com>
2025-01-30 10:23:54 -0500

    make options optional

--- app/models/room_message.rb
+++ app/models/room_message.rb
@@ -3,7 +3,7 @@ class RoomMessage < ApplicationRecord
 
   belongs_to :user
 
-  def as_json(options)
+  def as_json(options = {})
     super(options).merge(user_avatar_url: user.gravatar_url)
   end
 end
